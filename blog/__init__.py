0bb7d5c69777d87456577d88240b6d82d51daac9
Leeingnyo <dyd1928@naver.com>
2017-03-22 08:10:46 +0900

    add database module to connect database
    before you run server
    
    $ python
    >>> from blog.database import init_db
    >>> init_db()

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+
+from blog.database import db_session
+
 app = Flask(__name__)
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
 
 from blog.controllers import *
