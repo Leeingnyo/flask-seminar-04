a829613de770b6cc677083dc1fdc26659033faf0
Leeingnyo <dyd1928@naver.com>
2017-03-22 10:58:59 +0900

    add foreign key constraint, apply relationship between post, comment

--- blog/models/comment.py
+++ blog/models/comment.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, UnicodeText, DateTime
+from sqlalchemy import Column, Integer, UnicodeText, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from blog.database import Base
 
 class Comment(Base):
@@ -8,13 +9,20 @@ class Comment(Base):
     created_at = Column(DateTime, nullable=False)
     content = Column(UnicodeText, nullable=False)
     ip = Column(UnicodeText)
-    post_id = Column(Integer) # should i use forign key?
-    # relationship 과 backref 를 걸어주세요
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False) # should i use foreign key?
+    # you can use relationship without foreign key constraint
+    # see http://stackoverflow.com/questions/37806625/sqlalchemy-create-relations-but-without-foreign-key-constraint-in-db
 
-    def __init__(self, name, created_at, content, ip=None):
+    post = relationship('Post', backref=backref('comments', order_by=id))
+    # 'Post'는 Comment 처럼 Mapper 이름
+    # http://docs.sqlalchemy.org/en/latest/orm/relationship_api.html#sqlalchemy.orm.relationship
+    # http://docs.sqlalchemy.org/en/latest/orm/relationship_api.html#sqlalchemy.orm.backref
+
+    def __init__(self, name, created_at, content, post_id, ip=None):
         self.name = name
         self.created_at = created_at
         self.content = content
+        self.post_id = post_id
         self.ip = ip
 
     def __repr__(self):
